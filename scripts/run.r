7acc76a39db7d41d777e17336a6153903b34e2b1
Aleksandr Meshkov <agrobov@gmail.com>
2018-08-20 22:42:30 +0300

    Relative and absolute accuracy, percent error

--- scripts/run.r
+++ scripts/run.r
@@ -1,6 +1,6 @@
 source("scripts/eq_concentrations.r", chdir = TRUE)
 
-eq.conc.exec(sep = ";", subdir = "ds.2p", bs.name = "molecule2", verbose = TRUE)
-eq.conc.exec(sep = ";", subdir = "ds.3p", bs.name = "molecule2", verbose = TRUE)
-eq.conc.exec(sep = ";", subdir = "ds.5p", bs.name = "molecule4", verbose = TRUE)
-eq.conc.exec(sep = ";", subdir = "ds.eq", bs.name = "molecule3", verbose = TRUE)
+eq.conc.exec(sep = ";", subdir = "ds.2p", bs.name = "molecule2", thr.type = c("abs"), threshold = 1e-08, verbose = TRUE)
+eq.conc.exec(sep = ";", subdir = "ds.3p", bs.name = "molecule2", thr.type = c("abs"), threshold = 1e-08, verbose = TRUE)
+eq.conc.exec(sep = ";", subdir = "ds.5p", bs.name = "molecule4", thr.type = c("abs"), threshold = 1e-08, verbose = TRUE)
+eq.conc.exec(sep = ";", subdir = "ds.eq", bs.name = "molecule3", thr.type = c("abs"), threshold = 1e-08, verbose = TRUE)
