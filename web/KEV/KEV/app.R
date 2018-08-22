300b218426fee29554a1d4bda8c546061fab082f
Aleksandr Meshkov <agrobov@gmail.com>
2018-08-22 23:05:08 +0300

    Web: input

--- web/KEV/KEV/app.R
+++ web/KEV/KEV/app.R
@@ -53,22 +53,51 @@ ui <- navbarPage("KEV",
                             
                             , fluidRow(
                               
-                              column(12,
-                                     wellPanel(
-                                       h4("Upload input data or insert by hand")
-                                       , h4("Stechiometric coefficients")
-                                       , rHandsontableOutput("dt.coef")
-                                       , fileInput("file.dt.coef", "Choose CSV File",
-                                                   accept = c(
-                                                     "text/csv",
-                                                     "text/comma-separated-values,text/plain",
-                                                     ".csv")
-                                       )
-                                       , fluidRow(class = "download-row"
-                                                  , downloadButton("dt.coef.csv", "csv")
-                                                  , downloadButton("dt.coef.xlsx", "xlsx"))
-                                       
-                                     ) 
+                              column(
+                                12
+                                , wellPanel(
+                                  h4("Upload input data or insert by hand")
+                                  ,  fluidRow(
+                                    column(5
+                                           , h4("Stechiometric coefficients")
+                                           , rHandsontableOutput("dt.coef")
+                                           , fileInput("file.dt.coef", "Choose CSV File",
+                                                       accept = c(
+                                                         "text/csv",
+                                                         "text/comma-separated-values,text/plain",
+                                                         ".csv")
+                                           )
+                                           , fluidRow(class = "download-row"
+                                                      , downloadButton("dt.coef.csv", "csv")
+                                                      , downloadButton("dt.coef.xlsx", "xlsx")))
+                                    , column(2
+                                             , h4("K: lg constants")
+                                             , rHandsontableOutput("cnst")
+                                             , fileInput("file.cnst", "Choose CSV File",
+                                                         accept = c(
+                                                           "text/csv",
+                                                           "text/comma-separated-values,text/plain",
+                                                           ".csv")
+                                             )
+                                             , fluidRow(class = "download-row"
+                                                        , downloadButton("cnst.csv", "csv")
+                                                        , downloadButton("cnst.xlsx", "xlsx")))
+                                    , column(5
+                                             , h4("Concentrations")
+                                             , rHandsontableOutput("part.eq")
+                                             , rHandsontableOutput("dt.conc")
+                                             , fileInput("file.dt.conc", "Choose CSV File",
+                                                         accept = c(
+                                                           "text/csv",
+                                                           "text/comma-separated-values,text/plain",
+                                                           ".csv")
+                                             )
+                                             , fluidRow(class = "download-row"
+                                                        , downloadButton("dt.conc.csv", "csv")
+                                                        , downloadButton("dt.conc.xlsx", "xlsx")))
+                                    )
+                                  
+                                 ) 
                               )
                             )
                           )),
@@ -82,6 +111,8 @@ ui <- navbarPage("KEV",
 server <- function(input, output) {
    
   values <- reactiveValues()
+  
+  # data --------------------- #
   
   dt.coef.data <- reactive({
     
@@ -109,6 +140,89 @@ server <- function(input, output) {
     dt.coef
     
   })
+
+  dt.conc.data <- reactive({
+    
+    if (!is.null(input$dt.conc)) {
+      
+      dt.conc <- hot_to_r(input$dt.conc)
+      
+    } else {
+      
+      if (is.null(values[["dt.conc"]])) {
+        
+        dt.conc <- as.data.table(matrix(rep(1e-03, 20), ncol = 4))
+        setnames(dt.conc, paste0("molecule", 1:4))
+        
+      } else {
+        
+        dt.conc <- values[["dt.conc"]]
+        
+      }
+      
+    }
+    
+    values[["dt.conc"]] <- dt.conc
+    
+    dt.conc
+    
+  })
+
+  part.eq.data <- reactive({
+    
+    if (!is.null(input$part.eq)) {
+      
+      part.eq <- hot_to_r(input$part.eq)
+      
+    } else {
+      
+      if (is.null(values[["part.eq"]])) {
+        
+        part.eq <- as.data.table(matrix(rep("tot", 4), ncol = 4))
+        setnames(part.eq, paste0("molecule", 1:4))
+        
+      } else {
+        
+        part.eq <- values[["part.eq"]]
+        
+      }
+      
+    }
+    
+    values[["part.eq"]] <- part.eq
+    
+    part.eq
+    
+  })
+  
+  cnst.data <- reactive({
+    
+    if (!is.null(input$cnst)) {
+      
+      cnst <- hot_to_r(input$cnst)
+      
+    } else {
+      
+      if (is.null(values[["cnst"]])) {
+        
+        cnst <- as.data.table(matrix(rep(1, 4), ncol = 1))
+        setnames(cnst, "log10(K)")
+        
+      } else {
+        
+        cnst <- values[["cnst"]]
+        
+      }
+      
+    }
+    
+    values[["cnst"]] <- cnst
+    
+    cnst
+    
+  })
+  
+  # rendering ---------------- #
   
   output$dt.coef <- renderRHandsontable({
     
@@ -129,11 +243,72 @@ server <- function(input, output) {
     
   })
   
+  output$dt.conc <- renderRHandsontable({
+    
+    in.file <- input$file.dt.conc
+    
+    dt.conc <- dt.conc.data()
+    
+    if (!is.null(in.file)) {
+      
+      # browser()
+      dt.conc <- read.csv2(in.file$datapath, stringsAsFactors = FALSE, colClasses = "character", skip = 1)
+      
+    }
+    
+    if (!is.null(dt.conc))
+      rhandsontable(dt.conc, stretchH = "all", useTypes = FALSE) %>%
+      hot_context_menu(allowRowEdit = TRUE, allowColEdit = TRUE)
+    
+  })
+
+  output$part.eq <- renderRHandsontable({
+    
+    in.file <- input$file.part.eq
+    
+    part.eq <- part.eq.data()
+    
+    if (!is.null(in.file)) {
+      
+      # browser()
+      part.eq <- read.csv2(in.file$datapath, stringsAsFactors = FALSE, colClasses = "character", nrows = 1, header = FALSE)
+      tmp <- read.csv2(in.file$datapath, stringsAsFactors = FALSE, colClasses = "character", skip = 1, header = FALSE)
+      colnames(part.eq) <- colnames(tmp)
+
+    }
+    
+    if (!is.null(part.eq))
+      rhandsontable(part.eq, stretchH = "all", useTypes = FALSE) %>%
+      hot_context_menu(allowRowEdit = TRUE, allowColEdit = TRUE)
+    
+  })
+  
+  output$cnst <- renderRHandsontable({
+    
+    in.file <- input$file.cnst
+    
+    cnst <- cnst.data()
+    
+    if (!is.null(in.file)) {
+      
+      # browser()
+      cnst <- read.csv2(in.file$datapath, stringsAsFactors = FALSE, colClasses = "character")
+      
+    }
+    
+    if (!is.null(cnst))
+      rhandsontable(cnst, stretchH = "all", useTypes = FALSE) %>%
+      hot_context_menu(allowRowEdit = TRUE, allowColEdit = TRUE)
+    
+  })
+  
+  # downloads ---------------- #
+  
   output$dt.coef.csv <- downloadHandler(
     # ----
     filename = function() {
       
-      "dt.coef.csv"
+      "input_stechiometric_coefficients.csv"
       
     },
     
@@ -144,7 +319,41 @@ server <- function(input, output) {
     }
     
   )
+  # ----
   
+  output$cnst.csv <- downloadHandler(
+    # ----
+    filename = function() {
+      
+      "log10_K_constants.csv"
+      
+    },
+    
+    content = function(file) {
+      
+      write.csv2(cnst.data(), file, row.names = FALSE)
+      
+    }
+    
+  )
+  # ----
+  
+  output$dt.conc.csv <- downloadHandler(
+    # ----
+    filename = function() {
+      
+      "input_total_concentrations.csv"
+      
+    },
+    
+    content = function(file) {
+      
+      write.csv2(dt.conc.data(), file, row.names = FALSE)
+      
+    }
+
+  )
+  # ----
   
 }
 
