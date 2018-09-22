0724e3a9a59a59f619ac9d0f696061eb43980968
Aleksandr Meshkov <agrobov@gmail.com>
2018-09-22 22:57:43 +0300

    refactor absorbance evaluator + upgraded pattern move

--- app/kev/ab_evaluator.r
+++ app/kev/ab_evaluator.r
@@ -100,294 +100,362 @@ molar.ext.evaluator <- function(x.known = NULL, y.raw, dt.res.m, wght, method = 
 }
 
 
-# step function ------------------------------------------- #
 
-worker <- function(cnst.m, method = c("lm", "basic wls"), mode = c("iterator", "return", "debug")) {
-  
-  
-  # run equilibrium evaluator
-  
-  dt.res.m <- newton.wrapper(cnst.m, dt.coef.m, dt.conc.m, part.eq, reac.nm, "abs", 1e-08)
-  colnames(dt.res.m) <- dt.coef[, name]
-  
-  cnst.tune.nm <- which(colnames(dt.res.m) %in% cnst.tune)
 
-  #  run molar extinction evaluator
+# optimizer wrapper --------------------------------------- #
+
+constant.optimizer <- function(dt.coef, cnst.m, cnst.tune
+                               , dt.ab.m, dt.ab.err.m, dt.mol.m
+                               , dt.coef.m, dt.conc.m, part.eq, reac.nm
+                               , hardstop = 100
+                               , lrate.init = .5
+                               , search.density = 1
+                               , ab.threshold = 5e-5
+                               , mode = c("base", "grid", "debug")
+                               , method = c("lm", "basic wls")
+                               , algorithm = c("direct search", "basic search")) {
   
-  mol.coef <- data.table()
-  dt.ab.calc <- data.table()
   
-  for (i in 1:ncol(dt.ab.m)) {
+  # additional functions are defined inner the main optimizing function so not to pass lots of data as arguments
+  # time saved: 2.56 -> 2.07 on dsl.3 dataset
+  # sorry for monstruosity!
+  
+  # step function -------------------- #
+  
+  constant.error.evaluator <- function(cnst.m, method = c("lm", "basic wls"), mode = c("iterator", "return", "debug")) {
     
-    y.raw <- dt.ab.m[, i, drop = FALSE]
     
-    # weights for linear model
+    # run equilibrium evaluator
     
-    wght <- 1 / (dt.ab.err.m[, i] ^ 2)
+    dt.res.m <- newton.wrapper(cnst.m, dt.coef.m, dt.conc.m, part.eq, reac.nm, "abs", 1e-08)
+    colnames(dt.res.m) <- dt.coef[, name]
     
-    # if some molar coefficients are already known
+    cnst.tune.nm <- which(colnames(dt.res.m) %in% cnst.tune)
     
-    if (is.matrix(dt.mol.m)) {
+    #  run molar extinction evaluator
+    
+    mol.coef <- data.table()
+    dt.ab.calc <- data.table()
+    
+    for (i in 1:ncol(dt.ab.m)) {
       
-      x.known <- dt.mol.m[i, ]
-      rtrn <- molar.ext.evaluator(x.known, y.raw, dt.res.m, wght, method)
+      y.raw <- dt.ab.m[, i, drop = FALSE]
       
-    } else {
+      # weights for linear model
       
-      rtrn <- molar.ext.evaluator(NULL, y.raw, dt.res.m, wght, method)
+      wght <- 1 / (dt.ab.err.m[, i] ^ 2)
       
-    }
-
-    mol.coef <- rbind(mol.coef, as.data.table(as.list(rtrn$mol.coef)))
-    dt.ab.calc <- rbind(dt.ab.calc, as.data.table(as.list(rtrn$y.calc)))
-    
-  }
-
-  dt.ab.calc <- data.table(t(dt.ab.calc))
-  
-  
-  # evaluate cost function
-  
-  observed <- as.vector(dt.ab.m)
-  predicted <- as.vector(as.matrix(dt.ab.calc))
-  wght <- 1 / (as.vector(dt.ab.err.m) ^ 2)
-  
-  err <- sum(((observed - predicted) ^ 2) * wght)
-  
-  if (mode[1] == "iterator") {
-    
-    list(err = err)
-    
-  } else if (mode[1] == "return") {
-    
-    list(err = err, mol.coef = mol.coef)
-    
-  } else if (mode[1] == "debug") {
-    
-    list(err = err, mol.coef = mol.coef, err.v = (predicted - observed), dt.ab.calc = dt.ab.calc)
-    
-  }
-  
-  
-}
-
-
-worker.wrapper <- function(grid.opt, cnst.m, cnst.iter, step.iter, hardstop = 100
-                           , mode = c("base", "grid", "debug")
-                           , method = c("lm", "basic wls")
-                           , algorithm = c("direct search", "slow precise")) {
-  
-  for (j in 1:(hardstop * 10)) {
-    
-    cnst.back <- cnst.m[cnst.iter]
-    step.success <- grid.opt[closed == length(cnst.tune.nm), max(step.id)]
-    step.last <- grid.opt[, max(step.id)]
-    err.base <- grid.opt[step.success, err]
-    
-    # add current row
-    
-    if (step.iter != step.last) {
+      # if some molar coefficients are already known
       
-      grid.opt <- rbind(grid.opt, list(step.id = step.iter, step.type = "xpl", closed = 0), use.names = TRUE, fill = TRUE)
-      
-      if (grid.opt[step.success, step.type] == "xpl" & step.iter > 2 & algorithm[1] == "direct search"){
-
-        grid.opt[step.iter, step.type := "ptrn"]
-
-      }
-      
-      for (k in cnst.tune.nm) {
+      if (is.matrix(dt.mol.m)) {
         
-        lrate.tmp <- grid.opt[step.success, eval(as.name(paste0(k, "__lrate")))]
-        grid.opt[step.iter, eval(paste0(k, "__lrate")) := lrate.tmp]
-        grid.opt[step.iter, eval(as.character(k)) := cnst.m[k]]
-        
-      }
-      
-    }
-    
-    if (grid.opt[step.iter, step.type] == "xpl") {
-      
-      # exploratory move
-      
-      lrate <- grid.opt[step.iter, eval(as.name(paste0(cnst.iter, "__lrate")))]
-      
-      # if (step.iter == 2) {
-      #   
-      #   mdelta <- cnst.back  
-      #   
-      # } else {
-      #   
-      #   mdelta <- (cnst.back - grid.opt[step.iter - 2, eval(as.name(as.character(cnst.iter)))]) * 2
-      #   
-      # }
-      
-      dt.step <- data.table(cnst = cnst.back, err = err.base)
-      
-      for (i in 1:(search.density * 2)) {
-        
-        sgn <- i %% 2
-        if (sgn == 0) sgn <- -1
-        
-        cnst.curr <- cnst.back + lrate * ((i + 1) %/% 2) * sgn #* mdelta
-        cnst.m[cnst.iter] <- cnst.curr
-        
-        dt.step <- rbind(dt.step, list(cnst.curr, worker(cnst.m, method)$err), use.names = FALSE)
-        
-      }
-      
-      cnst.curr <- dt.step[err == min(err), cnst][1]
-      err.curr <- dt.step[, min(err)]
-      
-      if (mode[1] == "debug")
-        print(paste(step.iter, cnst.iter, cnst.curr, cnst.back, lrate))
-      
-      if (mode[1] == "grid") {
-        
-        grid.opt[step.iter, `:=`(err = dt.step[2, err], closed = closed + 1)]
-        grid.opt[step.iter, eval(as.character(cnst.iter)) := dt.step[2, cnst]]
-        cnst.m[cnst.iter] <- dt.step[2, cnst]
+        x.known <- dt.mol.m[i, ]
+        rtrn <- molar.ext.evaluator(x.known, y.raw, dt.res.m, wght, method)
         
       } else {
+        
+        rtrn <- molar.ext.evaluator(NULL, y.raw, dt.res.m, wght, method)
+        
+      }
+      
+      mol.coef <- rbind(mol.coef, as.data.table(as.list(rtrn$mol.coef)))
+      dt.ab.calc <- rbind(dt.ab.calc, as.data.table(as.list(rtrn$y.calc)))
+      
+    }
+    
+    dt.ab.calc <- data.table(t(dt.ab.calc))
+    
+    
+    # evaluate cost function
+    
+    observed <- as.vector(dt.ab.m)
+    predicted <- as.vector(as.matrix(dt.ab.calc))
+    wght <- 1 / (as.vector(dt.ab.err.m) ^ 2)
+    
+    err <- sum(((observed - predicted) ^ 2) * wght)
+    
+    if (mode[1] == "iterator") {
+      
+      list(err = err)
+      
+    } else if (mode[1] == "return") {
+      
+      list(err = err, mol.coef = mol.coef, dt.ab.calc = dt.ab.calc)
+      
+    } else if (mode[1] == "debug") {
+      
+      list(err = err, mol.coef = mol.coef, err.v = (predicted - observed), dt.ab.calc = dt.ab.calc)
+      
+    }
+    
+  }
+  
+  
+  # optimizer ------------------------ #
+  
+  constant.optimizer.inner <- function(grid.opt, cnst.m, cnst.tune.nm
+                                       , hardstop = 100
+                                       , mode = c("base", "grid", "debug")
+                                       , method = c("lm", "basic wls")
+                                       , algorithm = c("direct search", "basic search")) {
+    
+    # prepare values
+    
+    step.iter <- 2
+    cnst.iter <- cnst.tune.nm[1]
+    cnst.tune.wrk <- cnst.tune.nm
+    
+    # loop algorithms(s)
+    
+    for (j in 1:(hardstop * 10)) {
+      
+      cnst.back <- cnst.m[cnst.iter]
+      step.success <- grid.opt[closed >= length(cnst.tune.wrk), max(step.id)]
+      step.last <- grid.opt[, max(step.id)]
+      err.base <- grid.opt[step.success, err]
+      
+      # add current row
+      
+      if (step.iter != step.last) {
+        
+        grid.opt <- rbind(grid.opt, list(step.id = step.iter, step.type = "xpl", closed = 0), use.names = TRUE, fill = TRUE)
+        
+        if (grid.opt[step.success, step.type] == "xpl" & step.iter > 2 & algorithm[1] == "direct search"){
+          
+          grid.opt[step.iter, step.type := "ptrn"]
+          
+        }
+        
+        for (k in cnst.tune.nm) {
+          
+          lrate.tmp <- grid.opt[step.success, eval(as.name(paste0(k, "__lrate")))]
+          grid.opt[step.iter, eval(paste0(k, "__lrate")) := lrate.tmp]
+          grid.opt[step.iter, eval(as.character(k)) := cnst.m[k]]
+          
+        }
+        
+      }
+      
+      if (grid.opt[step.iter, step.type] == "xpl") {
+        
+        # exploratory move
+        
+        lrate <- grid.opt[step.iter, eval(as.name(paste0(cnst.iter, "__lrate")))]
+        
+        dt.step <- data.table(cnst = cnst.back, err = err.base)
+        
+        for (i in 1:(search.density * 2)) {
+          
+          sgn <- i %% 2
+          if (sgn == 0) sgn <- -1
+          
+          cnst.curr <- cnst.back + lrate * ((i + 1) %/% 2) * sgn #* mdelta
+          cnst.m[cnst.iter] <- cnst.curr
+          
+          dt.step <- rbind(dt.step
+                           , list(cnst.curr, constant.error.evaluator(cnst.m, method)$err)
+                           , use.names = FALSE)
+          
+        }
+        
+        cnst.curr <- dt.step[err == min(err), cnst][1]
+        err.curr <- dt.step[, min(err)]
+        
+        if (mode[1] == "debug")
+          print(paste(step.iter, cnst.iter, cnst.curr, cnst.back, lrate))
+        
+        if (mode[1] == "grid") {
+          
+          grid.opt[step.iter, `:=`(err = dt.step[2, err], closed = closed + 1)]
+          grid.opt[step.iter, eval(as.character(cnst.iter)) := dt.step[2, cnst]]
+          cnst.m[cnst.iter] <- dt.step[2, cnst]
+          
+        } else {
+          
+          # if (step.iter >= 112)
+          #   browser()
+          
+          if (err.curr < err.base) {
+            
+            grid.opt[step.iter, `:=`(err = err.curr, closed = closed + 1)]
+            grid.opt[step.iter, eval(as.character(cnst.iter)) := cnst.curr]
+            cnst.m[cnst.iter] <- cnst.curr
+            
+            # speed up if needed
+
+            if (nrow(grid.opt[step.type == "xpl"]) > 3) {
+
+              tmp <- grid.opt[step.type == "xpl", eval(as.name(paste0(cnst.iter, "__lrate")))]
+              tmp <- tmp[(length(tmp)-3):length(tmp)]
+              # browser()
+
+              if (max(tmp) == min(tmp)) {
+
+                grid.opt[step.iter, eval(paste0(cnst.iter, "__lrate")) := lrate * 2]
+
+              }
+
+            }
+            
+          } else {
+            
+            grid.opt[step.iter, eval(paste0(cnst.iter, "__lrate")) := lrate * .5]
+            cnst.m[cnst.iter] <- cnst.back
+            
+          }
+          
+        }
+        
+        tmp <- grid.opt[step.iter, c("step.id", paste0(cnst.tune.nm, "__lrate")), with = FALSE]
+        tmp <- melt(tmp, id.vars = "step.id", measure.vars = paste0(cnst.tune.nm, "__lrate"), variable.factor = FALSE)
+        
+        cnst.tune.wrk <- as.integer(str_extract(unlist(tmp[value > ab.threshold | is.na(value), variable]), "^[0-9]+"))
+        
+        if (length(cnst.tune.wrk) > 0) {
+          
+          cnst.iter <- which(cnst.tune.wrk == cnst.iter) + 1
+          
+          if (length(cnst.iter) == 0)
+            cnst.iter <- 1
+          
+          if (cnst.iter > length(cnst.tune.wrk)) {
+            
+            cnst.iter <- cnst.tune.wrk[1]
+            
+          } else {
+            
+            cnst.iter <- cnst.tune.wrk[cnst.iter]
+            
+          }
+          
+          if (grid.opt[step.iter, closed] == length(cnst.tune.wrk)) {
+            
+            step.iter <- step.iter + 1
+            
+          }
+          
+        }
+        
+      } else if (grid.opt[step.iter, step.type] == "ptrn") {
+        
+        # pattern move
+        
+        cnst.back <- cnst.m
+        
+        step.success.xpl.prev <- tail(which(grid.opt[, step.type] == "xpl"), 2)[1]
+        
+        cnst.m[cnst.tune.nm] <-
+          unlist(
+            grid.opt[step.iter, c(as.character(cnst.tune.nm)), with = FALSE] +
+              grid.opt[step.success, c(as.character(cnst.tune.nm)), with = FALSE] -
+              grid.opt[step.success.xpl.prev, c(as.character(cnst.tune.nm)), with = FALSE]
+          )
+        
+        err.curr <- constant.error.evaluator(cnst.m, method)$err
+        
+        # if (step.iter >= 113)
+        #   browser()
         
         if (err.curr < err.base) {
           
-          grid.opt[step.iter, `:=`(err = err.curr, closed = closed + 1)]
-          grid.opt[step.iter, eval(as.character(cnst.iter)) := cnst.curr]
-          cnst.m[cnst.iter] <- cnst.curr
+          grid.opt[step.iter, c(as.character(cnst.tune.nm))] <- as.list(cnst.m[cnst.tune.nm])
+          grid.opt[step.iter, err := err.curr]
           
         } else {
           
-          grid.opt[step.iter, eval(paste0(cnst.iter, "__lrate")) := lrate * .5]
-          cnst.m[cnst.iter] <- cnst.back
+          grid.opt[step.iter, c(as.character(cnst.tune.nm))] <- as.list(cnst.back[cnst.tune.nm])
+          grid.opt[step.iter, err := err.base]
+          cnst.m <- cnst.back
           
         }
         
-      }
-      
-      tmp <- grid.opt[step.iter, c("step.id", paste0(cnst.tune.nm, "__lrate")), with = FALSE]
-      tmp <- melt(tmp, id.vars = "step.id", measure.vars = paste0(cnst.tune.nm, "__lrate"), variable.factor = FALSE)
-      
-      cnst.tune.wrk <- as.integer(str_extract(unlist(tmp[value > ab.threshold | is.na(value), variable]), "^[0-9]+"))
-
-      if (length(cnst.tune.wrk) > 0) {
-
-        cnst.iter <- which(cnst.tune.wrk == cnst.iter) + 1
+        grid.opt[step.iter, closed := length(cnst.tune.wrk)]
         
-        if (length(cnst.iter) == 0)
-          cnst.iter <- 1
-        
-        if (cnst.iter > length(cnst.tune.wrk)) {
-          
-          cnst.iter <- cnst.tune.wrk[1]
-
-        } else {
-          
-          cnst.iter <- cnst.tune.wrk[cnst.iter]
-
-        }
-        
-        if (grid.opt[step.iter, closed] == length(cnst.tune.wrk)) {
-          step.iter <- step.iter + 1
-        }
+        step.iter <- step.iter + 1
         
       }
       
-    } else if (grid.opt[step.iter, step.type] == "ptrn") {
+      # check conditions
       
-      # pattern move
-      
-      cnst.back <- cnst.m
-      
-      step.success.xpl.prev <- tail(which(grid.opt[, step.type] == "xpl"), 2)[1]
-      
-      cnst.m[cnst.tune.nm] <-
-        unlist(
-          grid.opt[step.iter, c(as.character(cnst.tune.nm)), with = FALSE] +
-          grid.opt[step.success, c(as.character(cnst.tune.nm)), with = FALSE] -
-            grid.opt[step.success.xpl.prev, c(as.character(cnst.tune.nm)), with = FALSE]
-        )
-      
-      # cnst.m[cnst.tune.nm] <- unlist(grid.opt[step.iter, c(as.character(cnst.tune.nm)), with = FALSE])
-      
-      err.curr <- worker(cnst.m, method)$err
-      
-      if (err.curr < err.base) {
+      if (length(cnst.tune.wrk) == 0) {
         
-        grid.opt[step.iter, c(as.character(cnst.tune.nm))] <- as.list(cnst.m[cnst.tune.nm])
-        grid.opt[step.iter, err := err.curr]
-        
-      } else {
-        
-        grid.opt[step.iter, c(as.character(cnst.tune.nm))] <- as.list(cnst.back[cnst.tune.nm])
-        grid.opt[step.iter, err := err.base]
-        cnst.m <- cnst.back
+        break
         
       }
-      
-      grid.opt[step.iter, closed := length(cnst.tune.nm)]
-      step.iter <- step.iter + 1
       
     }
     
-    # check conditions
-
-    if (length(cnst.tune.wrk) == 0) {
+    
+    if (mode == "debug") {
       
-      break
-
+      rtrn <- constant.error.evaluator(cnst.m, method, mode = "debug")
+      list(grid.opt = grid.opt, cnst.m = cnst.m, mol.coef = rtrn$mol.coef, err.v = rtrn$err.v, dt.ab.calc = rtrn$dt.ab.calc)
+      
+    } else {
+      
+      rtrn <- constant.error.evaluator(cnst.m, method, mode = "return")
+      
+      dt.res.m <- newton.wrapper(cnst.m, dt.coef.m, dt.conc.m, part.eq, reac.nm, "abs", 1e-08)
+      colnames(dt.res.m) <- dt.coef[, name]
+      
+      list(grid.opt = grid.opt, cnst.m = cnst.m, mol.coef = rtrn$mol.coef, dt.ab.calc = rtrn$dt.ab.calc, dt.res.m = dt.res.m)
+      
     }
     
   }
   
   
-  if (mode == "debug") {
+  # get numbers of tuned constants
+  
+  cnst.tune.nm <- which(unlist(dt.coef[, name]) %in% cnst.tune)
+  
+  # create grid for optimizing
+  
+  grid.opt <- data.table(step.id = integer(), step.type = character(), err = numeric(), closed = integer())
+  
+  for (i in cnst.tune.nm) {
     
-    rtrn <- worker(cnst.m, method, mode = "debug")
-    list(grid.opt = grid.opt, cnst.m = cnst.m, mol.coef = rtrn$mol.coef, err.v = rtrn$err.v, dt.ab.calc = rtrn$dt.ab.calc)
-    
-  } else {
-    
-    rtrn <- worker(cnst.m, method, mode = "return")
-    list(grid.opt = grid.opt, cnst.m = cnst.m, mol.coef = rtrn$mol.coef)
+    grid.opt[, eval(as.character(i)) := numeric()]
+    grid.opt[, paste0(eval(i), "__lrate") := numeric()]
     
   }
   
-}
-
-
-# create grid --------------------------------------------- #
-
-dt.res.m <- newton.wrapper(cnst.m, dt.coef.m, dt.conc.m, part.eq, reac.nm, "rel", 1e-08)
-colnames(dt.res.m) <- dt.coef[, name]
-cnst.tune.nm <- which(unlist(dt.coef[, name]) %in% cnst.tune)
-
-grid.opt <- data.table(step.id = integer(), step.type = character(), err = numeric(), closed = integer())
-
-for (i in cnst.tune.nm) {
+  # create first row
   
-  grid.opt[, eval(as.character(i)) := numeric()]
-  grid.opt[, paste0(eval(i), "__lrate") := numeric()]
+  grid.opt <- rbind(grid.opt, list(step.id = 1, step.type = "xpl", closed = length(cnst.tune.nm)), use.names = TRUE, fill = TRUE)
   
-}
-
-# add row
-
-grid.opt <- rbind(grid.opt, list(step.id = 1, step.type = "xpl", closed = length(cnst.tune.nm)), use.names = TRUE, fill = TRUE)
-
-for (i in cnst.tune.nm) {
+  for (i in cnst.tune.nm) {
+    
+    grid.opt[step.id == 1, eval(as.character(i)) := cnst.m[i]]
+    grid.opt[, eval(paste0(i, "__lrate")) := lrate.init * eval(as.name(as.character(i)))]
+    
+  }
   
-  grid.opt[step.id == 1, eval(as.character(i)) := cnst.m[i]]
-  grid.opt[, eval(paste0(i, "__lrate")) := lrate.init * eval(as.name(as.character(i)))]
+  # get starting error
+  
+  err.v <- constant.error.evaluator(cnst.m, method = "lm", mode = "return")$err
+  grid.opt[, err := err.v]
+  
+  # run optimizer
+  
+  rtrn <- constant.optimizer.inner(grid.opt, cnst.m, cnst.tune.nm
+                                   , hardstop, mode, method, algorithm)
+  
+  rtrn
   
 }
 
-err.v <- worker(cnst.m, method = "lm", mode = "return")$err
-grid.opt[, err := err.v]
 
 
 remove(res)
-system.time(res <- worker.wrapper(grid.opt, cnst.m, 3, 2, hardstop = 400, mode = "base", method = "basic wls", algorithm = "slow precise"))
+system.time(res <- constant.optimizer(dt.coef, cnst.m, cnst.tune = c("HL", "H2L")
+                                      , dt.ab.m, dt.ab.err.m, dt.mol.m
+                                      , dt.coef.m, dt.conc.m, part.eq, reac.nm
+                                      , hardstop = 400
+                                      , lrate.init = .5
+                                      , search.density = 2
+                                      , ab.threshold = 5e-5
+                                      , mode = "base", method = "basic wls", algorithm = "direct search"))
 res
 
 
