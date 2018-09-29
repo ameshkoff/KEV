dd824b9f1eed1e3507a1b06bd684afe9a4343ad0
Aleksandr Meshkov <agrobov@gmail.com>
2018-09-29 12:18:29 +0300

    minor bug with saved file names

--- app/kev/ab_save.r
+++ app/kev/ab_save.r
@@ -20,38 +20,38 @@ ab.save <- function(subdir, sep, dt.res, dt.ab.calc, ab.res.abs, ab.res.rel, ab.
   if (sep == ";") {
     
     write.csv2(dt.res, file = paste0("output", subdir, "equilibrium_concentrations.csv"), row.names = FALSE)
-    write.csv2(dt.ab.calc, file = paste0("output", subdir, "_absorbance.csv"), row.names = FALSE)
+    write.csv2(dt.ab.calc, file = paste0("output", subdir, "absorbance.csv"), row.names = FALSE)
     write.csv2(ab.res.abs, file = paste0("output", subdir, "absorbance_st_deviations_absolute.csv"), row.names = FALSE)
     write.csv2(ab.res.rel, file = paste0("output", subdir, "absorbance_st_deviations_relative.csv"), row.names = FALSE)
-    write.csv2(scl, file = paste0("output", subdir, "_fmin_last_diff.csv"), row.names = FALSE)
+    write.csv2(scl, file = paste0("output", subdir, "fmin_last_diff.csv"), row.names = FALSE)
     write.csv2(cnst.dev, file = paste0("output", subdir, "constants_with_st_deviations.csv"), row.names = FALSE)
     write.csv2(cor.m, file = paste0("output", subdir, "correlation_matrix.csv"), row.names = FALSE)
-    write.csv2(mol.coef, file = paste0("output", subdir, "_molar_extinction_coefficients.csv"), row.names = FALSE)
-    write.csv2(mol.coef, file = paste0("output", subdir, "_molar_extinction_coefficients.csv"), row.names = FALSE)
+    write.csv2(mol.coef, file = paste0("output", subdir, "molar_extinction_coefficients.csv"), row.names = FALSE)
+    write.csv2(mol.coef, file = paste0("output", subdir, "molar_extinction_coefficients.csv"), row.names = FALSE)
 
   } else if (sep == ",") {
     
     write.csv(dt.res, file = paste0("output", subdir, "equilibrium_concentrations.csv"), row.names = FALSE)
-    write.csv(dt.ab.calc, file = paste0("output", subdir, "_absorbance.csv"), row.names = FALSE)
+    write.csv(dt.ab.calc, file = paste0("output", subdir, "absorbance.csv"), row.names = FALSE)
     write.csv(ab.res.abs, file = paste0("output", subdir, "absorbance_st_deviations_absolute.csv"), row.names = FALSE)
     write.csv(ab.res.rel, file = paste0("output", subdir, "absorbance_st_deviations_relative.csv"), row.names = FALSE)
-    write.csv(scl, file = paste0("output", subdir, "_fmin_last_diff.csv"), row.names = FALSE)
+    write.csv(scl, file = paste0("output", subdir, "fmin_last_diff.csv"), row.names = FALSE)
     write.csv(cnst.dev, file = paste0("output", subdir, "constants_with_st_deviations.csv"), row.names = FALSE)
     write.csv(cor.m, file = paste0("output", subdir, "correlation_matrix.csv"), row.names = FALSE)
-    write.csv(mol.coef, file = paste0("output", subdir, "_molar_extinction_coefficients.csv"), row.names = FALSE)
-    write.csv(mol.coef, file = paste0("output", subdir, "_molar_extinction_coefficients.csv"), row.names = FALSE)
+    write.csv(mol.coef, file = paste0("output", subdir, "molar_extinction_coefficients.csv"), row.names = FALSE)
+    write.csv(mol.coef, file = paste0("output", subdir, "molar_extinction_coefficients.csv"), row.names = FALSE)
     
   } else if (sep == "tab") {
     
     write.table(dt.res, file = paste0("output", subdir, "equilibrium_concentrations.csv"), sep = "\t", row.names = FALSE)
-    write.table(dt.ab.calc, file = paste0("output", subdir, "_absorbance.csv"), sep = "\t", row.names = FALSE)
+    write.table(dt.ab.calc, file = paste0("output", subdir, "absorbance.csv"), sep = "\t", row.names = FALSE)
     write.table(ab.res.abs, file = paste0("output", subdir, "absorbance_st_deviations_absolute.csv"), sep = "\t", row.names = FALSE)
     write.table(ab.res.rel, file = paste0("output", subdir, "absorbance_st_deviations_relative.csv"), sep = "\t", row.names = FALSE)
-    write.table(scl, file = paste0("output", subdir, "_fmin_last_diff.csv"), sep = "\t", row.names = FALSE)
+    write.table(scl, file = paste0("output", subdir, "fmin_last_diff.csv"), sep = "\t", row.names = FALSE)
     write.table(cnst.dev, file = paste0("output", subdir, "constants_with_st_deviations.csv"), sep = "\t", row.names = FALSE)
     write.table(cor.m, file = paste0("output", subdir, "correlation_matrix.csv"), sep = "\t", row.names = FALSE)
-    write.table(mol.coef, file = paste0("output", subdir, "_molar_extinction_coefficients.csv"), sep = "\t", row.names = FALSE)
-    write.table(mol.coef, file = paste0("output", subdir, "_molar_extinction_coefficients.csv"), sep = "\t", row.names = FALSE)
+    write.table(mol.coef, file = paste0("output", subdir, "molar_extinction_coefficients.csv"), sep = "\t", row.names = FALSE)
+    write.table(mol.coef, file = paste0("output", subdir, "molar_extinction_coefficients.csv"), sep = "\t", row.names = FALSE)
     
   }
   
