1d04e0da970ea868b57e3584f9eae09dfb6678b9
Aleksandr Meshkov <agrobov@gmail.com>
2018-11-07 23:20:37 +0300

    register corrected

---
+++ app/kev/eq_save.r
@@ -0,0 +1,38 @@
+# ########################################################## #
+#                                                            #
+# Name: KEV:Constant Evaluator                               #
+# Author: AMeshkov                                           #
+# Date: 2018                                                 #
+#                                                            #
+# ########################################################## #
+
+
+
+eq.save <- function(subdir, sep, dt.res, dt.frac, dt.err, bs.name) {
+  
+  if (subdir != "")
+    subdir <- paste0("/", subdir, "/")
+  
+  dir.create(file.path(paste0("output", subdir)), showWarnings = FALSE)
+  
+  if (sep == ";") {
+    
+    write.csv2(dt.res, file = paste0("output", subdir, "equilibrium_concentrations.csv"))
+    write.csv2(dt.frac, file = paste0("output", subdir, bs.name, "_fractions.csv"))
+    write.csv2(dt.err, file = paste0("output", subdir, "percent_error.csv"))
+    
+  } else if (sep == ",") {
+    
+    write.csv(dt.res, file = paste0("output", subdir, "equilibrium_concentrations.csv"))
+    write.csv(dt.frac, file = paste0("output", subdir, bs.name, "_fractions.csv"))
+    write.csv(dt.err, file = paste0("output", subdir, "percent_error.csv"))
+    
+  } else if (sep == "tab") {
+    
+    write.table(dt.res, file = paste0("output", subdir, "equilibrium_concentrations.txt"), sep = "\t")
+    write.table(dt.frac, file = paste0("output", subdir, bs.name, "_fractions.txt"), sep = "\t")
+    write.table(dt.err, file = paste0("output", subdir, "percent_error.txt"), sep = "\t")
+    
+  }
+  
+}
