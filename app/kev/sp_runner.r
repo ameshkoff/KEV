47603721aec1154af7a34ed079bfa9bba926c5b2
Aleksandr Meshkov <agrobov@gmail.com>
2018-11-10 23:02:01 +0300

    molar extinction coefficients from spectra

---
+++ app/kev/sp_runner.r
@@ -0,0 +1,97 @@
+# ########################################################## #
+#                                                            #
+# Name: KEV:Constant Evaluator                               #
+# Author: AMeshkov                                           #
+# Date: 2018                                                 #
+#                                                            #
+# ########################################################## #
+
+
+
+# ---------------------- load libraries ----------------------
+
+# I/O
+# data structure
+library(data.table)
+# computation
+library(MASS)
+library(Matrix)
+library(Hmisc)
+# strings
+library(stringi)
+library(stringr)
+
+
+
+# runner -------------------------------------------- #
+
+sp.evaluation.runner <- function(mode = c("api", "script", "app")
+                                 , sep = "tab"
+                                 , subdir = ""
+                                 , save.res = TRUE) {
+  
+  # source code ------------- #
+  
+  dir.start <- ""
+  
+  if (mode %in% c("script", "api"))
+    dir.start <- "app/KEV/"
+  
+  source(paste0(dir.start, "sp_data.r"), chdir = TRUE)
+  source(paste0(dir.start, "sp_preproc.r"), chdir = TRUE)
+  source(paste0(dir.start, "sp_evaluator.r"), chdir = TRUE)
+  source(paste0(dir.start, "sp_postproc.r"), chdir = TRUE)
+  source(paste0(dir.start, "sp_save.r"), chdir = TRUE)
+  
+  
+  # load data ---------------- #
+  
+  if (mode[1] == "script") {
+    
+    dt.sp <- sp.scripts.load(sep, subdir)
+    
+  } else if (mode[1] %in% c("app", "api")) {
+    
+    dt.sp <- dt.list
+    
+  }
+  
+  
+  # preproc data --------------- #
+  
+  dt.sp <- sp.preproc(dt.sp)
+  
+  
+  # run evaluator --------------- #
+  
+  system.time(dt.ttl <- spectra.mol.ext.evaluator(dt.sp))
+  
+  
+  # postprocessing ---------------- #
+  
+  dt.mol.full <- sp.postproc(dt.ttl)
+  
+  
+  # save -------------------------- #
+  
+  if (mode == "script" & save.res) {
+    
+    sp.save(subdir, sep, dt.mol.full)
+    
+  }
+  
+  # return
+  
+  dt.mol.full
+
+}
+
+
+
+
+
+
+
+
+
+
