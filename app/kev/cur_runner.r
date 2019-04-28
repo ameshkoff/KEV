eccbd76f76984f65c8c226bcaf5e4c7208aeac29
Aleksandr Meshkov <agrobov@gmail.com>
2019-04-28 14:42:09 +0300

    preprocessing

--- app/kev/cur_runner.r
+++ app/kev/cur_runner.r
@@ -41,7 +41,7 @@ if (mode[1] %in% c("script", "api"))
   dir.start <- "app/KEV/"
 
 source(paste0(dir.start, "cur_data.r"), chdir = TRUE)
-# source(paste0(dir.start, "cur_preproc.r"), chdir = TRUE)
+source(paste0(dir.start, "cur_preproc.r"), chdir = TRUE)
 # source(paste0(dir.start, "cur_evaluator.r"), chdir = TRUE)
 # source(paste0(dir.start, "cur_postproc.r"), chdir = TRUE)
 # source(paste0(dir.start, "cur_save.r"), chdir = TRUE)
@@ -59,6 +59,18 @@ if (mode[1] == "script") {
   dt.ttl <- dt.list
   
 }
+
+# preproc data --------------- #
+
+dt.ttl <- cur.preproc(dt.ttl)
+
+dt.cur <- dt.ttl[["dt.cur"]]
+cur.task <- dt.ttl[["cur.task"]]
+border.left <- dt.ttl[["border.left"]]
+border.right <- dt.ttl[["border.right"]]
+dt.par <- dt.ttl[["dt.par"]]
+
+
 
 
 
