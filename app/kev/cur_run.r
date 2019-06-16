8373fd93ed6a86d9d8a5d9e5578a861d2ed71418
Aleksandr Meshkov <agrobov@gmail.com>
2019-06-16 19:01:24 +0300

    curves: minor refactoring

---
+++ app/kev/cur_run.r
@@ -0,0 +1,41 @@
+# ########################################################## #
+#                                                            #
+# Name: KEV:Constant Evaluator                               #
+# Author: AMeshkov                                           #
+# Date: 2019                                                 #
+#                                                            #
+# ########################################################## #
+
+
+
+source("app/KEV/cur_runner.r", chdir = TRUE)
+
+
+# run loading & preprocessing --------------------------------
+
+cur.status <- cur.data.runner(mode = "script"
+                              , sep = ";"
+                              , subdir = "curves/dsc.1.no.assumptions/semicolon"
+                              , file = NULL
+                              , save.res = TRUE
+                              , dt.list = NULL)
+
+
+# run modelling ----------------------------------------------
+
+cur.status@window.borders[1] <- 210
+# cur.status@window.borders[2] <- 440
+
+cur.status <- cur.remove.curves(cur.status, min.expvalue = 200)
+
+cur.status <- cur.model(cur.status)
+
+
+
+
+
+cur.plot.initial(cur.status)
+
+cur.plot.model(cur.status)
+
+cur.status@metrics$r.squared
