dcab05400de47478f9a3f1d8c82ed845c7478eba
Aleksandr Meshkov <agrobov@gmail.com>
2020-01-26 14:18:08 +0300

    auto tests : stat tests

--- tests/unit.tests/tests/ht_stat.r
+++ tests/unit.tests/tests/ht_stat.r
@@ -19,9 +19,30 @@ context(kev.context)
 
 test_that("calorimetry_output_r.squared", {
   
-  if (!(str_to_lower(kev.context) %like% "(\\b|\\_)(bad)\\b"))
+  if ((str_to_lower(kev.context) %like% "(\\b|\\_)(bad)\\b")) {
+    
+    expect_lt(rtrn$adj.r.squared, .99)
+    
+  } else {
+    
     expect_gt(rtrn$adj.r.squared, .99)
+    
+  }
 
+})
+
+test_that("calorimetry_output_validity", {
+  
+  if (str_to_lower(kev.context) %like% "(\\b|\\_)(bad)\\b" & !(str_to_lower(kev.context) %like% "(\\b|\\_)(no(\\_|\\.)opt)\\b")) {
+    
+    expect_gt(nrow(rtrn$cnst.dev[validity != "OK" | is.na(validity)]), 0)
+    
+  } else if (!(str_to_lower(kev.context) %like% "(\\b|\\_)(no(\\_|\\.)opt)\\b")) {
+    
+    expect_equal(nrow(rtrn$cnst.dev[validity != "OK" | is.na(validity)]), 0)
+    
+  }
+  
 })
 
 
