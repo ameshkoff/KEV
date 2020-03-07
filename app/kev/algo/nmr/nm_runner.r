1b3527c88ed2967ea3d982ee08b3b64ac4523133
Aleksandr Meshkov <agrobov@gmail.com>
2020-03-07 21:46:08 +0300

    samples : checked & nmr : regressive bug fixed

--- app/kev/algo/nmr/nm_runner.r
+++ app/kev/algo/nmr/nm_runner.r
@@ -211,7 +211,7 @@ nm.evaluation.runner <- function(mode = c("api", "script", "app")
 
   for (i in tbl) {
     
-    dt <- dt.nm.calc[, .(signal, solution, calculated = get(i))]
+    dt <- dt.nm.calc[, .(signal, solution, calculated = as.vector(get(i)))]
     assign(i, dt)
     
   }
